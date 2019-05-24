5ec58f7e522f9d974d4952d6e736c92924f9ce94
Eric Bottard <ebottard@pivotal.io>
2019-05-24 16:05:09 +0200

    Initial import

---
+++ src/main/java/com/example/LiiklusClient.java
@@ -0,0 +1,107 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.github.bsideup.liiklus.protocol.Assignment;
+import com.github.bsideup.liiklus.protocol.PublishRequest;
+import com.github.bsideup.liiklus.protocol.ReactorLiiklusServiceGrpc;
+import com.github.bsideup.liiklus.protocol.ReceiveReply;
+import com.github.bsideup.liiklus.protocol.ReceiveRequest;
+import com.github.bsideup.liiklus.protocol.SubscribeReply;
+import com.github.bsideup.liiklus.protocol.SubscribeRequest;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
+import io.projectriff.processor.serialization.Message;
+import reactor.core.publisher.Flux;
+
+public class LiiklusClient {
+
+
+	public static void main(String[] args) {
+		if (args.length != 3) {
+			usage("Usage: LiiklusClient [--producer|--consumer] [liiklus-host:port] [stream-name]");
+		}
+		String liiklusTarget = args[1];
+
+		var channel = NettyChannelBuilder.forTarget(liiklusTarget)
+				.directExecutor()
+				.usePlaintext()
+				.build();
+
+		var stub = ReactorLiiklusServiceGrpc.newReactorStub(channel);
+
+		switch (args[0]) {
+			case "--producer":
+				Flux.<String>create(flux -> {
+					while (true) {
+						try {
+							String payload = new BufferedReader(new InputStreamReader(System.in)).readLine();
+							flux.next(payload);
+						}
+						catch (IOException e) {
+							flux.error(e);
+						}
+					}
+				}).concatMap(it -> stub.publish(
+					PublishRequest.newBuilder()
+							.setTopic(args[2])
+							.setKey(ByteString.copyFromUtf8("irrelevant"))
+							.setValue(stringAsMessage(it))
+							.build()
+			))
+						.blockLast();
+
+				break;
+
+			case "--consumer":
+				Flux.just(args[2])
+						.flatMap(topic ->
+								stub.subscribe(subscribeRequestFor(topic))
+										.filter(SubscribeReply::hasAssignment)
+										.map(SubscribeReply::getAssignment)
+										.map(LiiklusClient::receiveRequestForAssignment)
+										.flatMap(stub::receive)
+										.doOnNext(rr -> System.out.format("%s: %s%n", topic, extractRiffMessage(rr).getPayload().toStringUtf8()))
+						).blockLast();
+				break;
+			default:
+				usage("Usage: TestClient [--producer|--consumer] [liiklus-host] [stream-name]");
+		}
+	}
+
+	private static Message extractRiffMessage(ReceiveReply rr) {
+		try {
+			return Message.parseFrom(rr.getRecord().getValue());
+		} catch (InvalidProtocolBufferException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static SubscribeRequest subscribeRequestFor(String topic) {
+		return SubscribeRequest.newBuilder()
+				.setTopic(topic)
+				.setGroup("my-group")
+				.setAutoOffsetReset(SubscribeRequest.AutoOffsetReset.LATEST)
+				.build();
+	}
+
+	private static ReceiveRequest receiveRequestForAssignment(Assignment assignment) {
+		return ReceiveRequest.newBuilder().setAssignment(assignment).build();
+	}
+
+
+	private static ByteString stringAsMessage(String it) {
+		return Message.newBuilder()
+				.setPayload(ByteString.copyFromUtf8(it))
+				.setContentType("text/plain")
+				.build().toByteString();
+	}
+
+	private static void usage(String x) {
+		System.err.println(x);
+		System.exit(1);
+	}
+}
